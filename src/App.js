774bf5ca7558e27c80614de373b89652181ff9cb
realolejka <46599264+realolejka@users.noreply.github.com>
2020-02-23 16:43:47 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -5,66 +5,86 @@ import List from './List';
 import List2 from './list2';*/
 import "./App.css"
 import Laba2 from './Laba2';
+import Laba4 from './Laba4';
 
 
 class App extends Component {
-  constructor(props) {
+
+   constructor(props) {
+     
     super(props);
-      this.state = {
-        selectedFile: null,
-        showFile1: false,
-      }
-   
+     this.state ={
+       showContent: false,
+       counter: 0
+     }
+     this.counterContent = this.counterContent.bind(this)
+     this.toggleContent  = this.toggleContent.bind(this)
+     this.setCounter0 = this.setCounter0.bind(this)
   }
  
-  onChangeHandler=event=>{
-    this.setState({ 
-      selectedFile: event.target.files[0],
-      loaded: 0,
+  toggleContent (event){
+    event.preventDefault()
+    this.setState({
+      showContent: !this.state.showContent
+    })
+
+  }  
+
+
+
+  counterContent(event){
+    event.preventDefault()
+    
+    this.setState({
+      counter: this.state.counter +1 
     })
   }
-
-
-onClickHanlder = (e) => {
-    this.setState ({showFile1: true});
-   /* switch(e.target.id){
-      case 'laba1': this.setState({ arLab: [true, false, false, false, false, false, false, false,false, false]});
-      break;
-      case 'laba2': this.setState({ arLab: [false, true, false, false, false, false, false, false,false, false]});
-      break;
-    }*/
-}
+  
+  setCounter0(event){
+    event.preventDefault()
+    this.setState({
+      counter: this.state.counter == 0
+    })
+  }
+  passParametr(data_num){
+    
+  }
+  
 
   render(){
-    const { showFile1 } = this.state;
-
+    const {showContent} = this.state;
+    const {counter} = this.state;
+    
+    
+    
   return (
     <div className="myApp">
       <div className="LeftWindow">
-        <p className="title" >Oleg Arsenov IPZ-17-1 ReactJS labs</p>
+        <p className="title " onClick={this.setCounter0} >Oleg Arsenov IPZ-17-1 ReactJS labs</p>
         <nav id = "links">
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 1</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 2</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 3</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 4</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 5</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 6</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 7</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 8</div>
-          <div className="nav-link active-link"data-num="0">Лабораторная работа 9</div>
-
-
-
+          <div className="nav-link active-link" data_num="1" >Лабораторная работа 1 </div>
+          <div className="nav-link active-link" data_num="2" onClick={this.toggleContent}>Лабораторная работа 2 </div>
+          <div className="nav-link active-link" data_num="3" onClick={this.counterContent}>Лабораторная работа 3</div>
+          <div className="nav-link active-link" data_num="4" onClick={this.counterContent}>Лабораторная работа 4</div>
+          <div className="nav-link active-link" data_num="5">Лабораторная работа 5</div>
+          <div className="nav-link active-link" data_num="6">Лабораторная работа 6</div>
+          <div className="nav-link active-link" data_num="7">Лабораторная работа 7</div>
+          <div className="nav-link active-link" data_num="8">Лабораторная работа 8</div>
+          <div className="nav-link active-link" data_num="9">Лабораторная работа 9</div>
         </nav>
-        
 
       </div>
       <div className="RightWindow">
         <div className="Top">
-          {showFile1 === true ? <Laba2 /> : ''}
+        
+        {counter === 3 ? <Laba2 />:""}
+        {counter === 4 ? <Laba4 />:""}
+
+        
+       
         </div>
         <div className="Bottom">
-          <input name="uploadFile" type="file" />
+          
         </div>
       </div>
     </div>
---
+++ src/Laba4.js
@@ -0,0 +1,5 @@
+import React  from 'react';
+
+const Laba4 = (i) => <h1> Laba4 </h1>
+
+export default Laba4;
